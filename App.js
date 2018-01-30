b056563aa20ec94e28506e514ab7f77793760a64
Hampton <hpbrew@gmail.com>
2018-01-29 22:17:30 -0600

    Added text input

--- App.js
+++ App.js
@@ -1,3 +1,113 @@
+import React, { Component } from 'react';
+import {
+  Alert, AppRegistry, Platform, StyleSheet,
+  Text, TouchableHighlight, TouchableOpacity,
+  TouchableNativeFeedback, TouchableWithoutFeedback,
+  View, ScrollView, TextInput, Slider, StatusBar
+} from 'react-native';
+import { AutoGrowingTextInput } from 'react-native-autogrow-textinput';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { textValue: 'My initial\nText' };
+  }
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    Alert.alert('You long-pressed the button!')
+  }
+
+  _onChange(event) {
+    this.setState({ textValue: event.nativeEvent.text || '' });
+  }
+
+  _resetTextInput() {
+    this._textInput.clear();
+    this._textInput.resetHeightToMin();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.textInputContainer}>
+          <AutoGrowingTextInput value={this.state.textValue}
+            onChange={(event) => this._onChange(event)}
+            style={styles.textInput}
+            placeholder={'Your Message'}
+            placeholderTextColor='#66737C'
+            maxHeight={200}
+            minHeight={45}
+            enableScrollToCaret
+            ref={(r) => { this._textInput = r; }} />
+          {/* <ScrollView style={{ flex: 1 }}>
+          <TextInput
+            style={{
+              backgroundColor: 'white',
+              borderColor: 'gray',
+            }}
+            placeholder="Type here!"
+            onChangeText={(text) => this.setState({ text })}
+            multiline={true}
+            numberOfLines={4}
+          />
+
+        </ScrollView> */}
+        </View>
+        <View style={{ flex: 2, backgroundColor: 'yellow' }}>
+
+        </View>
+        <View style={{ flex: 3, backgroundColor: 'green' }}>
+          <TouchableOpacity style={styles.button}
+            onPress={() => this._resetTextInput()}>
+            <Text>Clear</Text>
+          </TouchableOpacity>
+        </View>
+      </View >
+    );
+  }
+}
+const IsIOS = Platform.OS === 'ios';
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    flex: 1,
+    backgroundColor: '#979da8'
+  },
+  textInputContainer: {
+    flexDirection: 'row',
+    paddingLeft: 8,
+    paddingRight: 8
+  },
+  welcome: {
+    marginTop: 100,
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  textInput: {
+    paddingLeft: 10,
+    fontSize: 17,
+    flex: 1,
+    backgroundColor: 'white',
+    borderWidth: 0,
+    borderRadius: IsIOS ? 4 : 0,
+  },
+  button: {
+    paddingLeft: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  }
+})
+
+/**
+
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Container, Content, Card, CardItem, Body } from 'native-base'
@@ -35,3 +145,6 @@ export default class App extends Component {
 //     justifyContent: 'center',
 //   },
 // })
+
+ */
+
